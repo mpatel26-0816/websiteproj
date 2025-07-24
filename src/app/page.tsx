44c08efab0fcb780b025a22a809ba9869ac65691
mpatel26-0816 <patelmudra2004@gmail.com>
2025-07-24 02:52:42 -0700

    Loading Animation complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <main className="relative">
       <section id="home" className="h-screen w-full relative">
 
-        <Image src="/landingpage.jpg" alt="background image" layout="fill" objectFit="cover" className="-z-10" quality={100} priority sizes="100vw"/>
-        <div className="absolute top-1/5 left-1/2 -translate-x-1/2 w-full max-w-lg px-4 text-center text-white md:top-1/5 md:bottom-auto md:left-19/80 md:-translate-x-5/12 md:text-left md:w-auto md:max-w-none md:px-0">
+        <Image src="/landingpage.jpg" alt="background image" layout="fill" objectFit="cover" className="opacity-0 fade-in-image -z-10" quality={100} priority sizes="100vw "/>
+        <div className="opacity-0 animate-[fadeInRight_1s_ease-out_0.33s_forwards] absolute top-1/5 left-1/2 -translate-x-1/2 w-full max-w-lg px-4 text-center text-white md:top-1/5 md:bottom-auto md:left-[calc(23.75%+20px)] md:-translate-x-5/12 md:text-left md:w-auto md:max-w-none md:px-0">
           <h1 className="hidden md:block text-6xl md:text-9xl font-extrabold leading-none">
           Mudra<br />
           Patel
@@ -22,7 +22,7 @@ export default function Home() {
           </h1> 
         </div>
 
-        <div className="absolute top-1/4 left-1/2 -translate-x-1/2 w-11/12 text-center text-white md:top-auto md:left-11/20 md:bottom-15/48 md:right-20 md:w-auto md:translate-x-0 md:text-left">
+        <div className="opacity-0 animate-[fadeInLeft_1s_ease-out_0.46s_forwards] absolute top-1/4 left-1/2 -translate-x-1/2 w-11/12 text-center text-white md:top-auto md:left-11/20 md:bottom-15/48 md:right-20 md:w-auto md:translate-x-0 md:text-left">
           <p className="hidden md:block text-2xl font-semibold">
             Personal Statement Undecided <br />
             something tech
@@ -32,7 +32,7 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 flex justify-center text-white"> {/*for the pointer arrow (now sure if keeping) */}
+        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 flex justify-center text-white opacity-0.5 fade-in-image"> {/*for the pointer arrow (now sure if keeping) */}
           <p className="font-semibold text-white animate-bounce absolute bottom-6 left-1/2 -translate-x-1/2 flex justify-center whitespace-nowrap">
             scroll below
           </p>
