2d59dda2c4dc3d98bf599f69f8859655477efa2c
mpatel26-0816 <patelmudra2004@gmail.com>
2025-07-30 18:48:48 -0700

    deploy test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "react/no-unescaped-entities": [
+        "error",
+        {
+          forbid: [">", "}"], // allow apostrophes and quotes
+        },
+      ],
+    },
+  },
 ];
-
-export default eslintConfig;
